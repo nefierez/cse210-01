b8043954b171cedcb4e6c69d6d2cbd563ce2d7a8
nefierez <nefierez@byui.edu>
2022-10-05 18:17:22 -0600

    Adding the seeker games to my local repository

--- dice-incomplete/dice/game/die.py
+++ dice-incomplete/dice/game/die.py
@@ -4,6 +4,7 @@ import random
 # TODO: Implement the Die class as follows...
 
 # 1) Add the class declaration. Use the following class comment.
+class Die:
     """A small cube with a different number of spots on each of its six sides.
 
     The responsibility of Die is to keep track of the side facing up and calculate the points for 
@@ -15,15 +16,21 @@ import random
     """
 
 # 2) Create the class constructor. Use the following method comment.
+    def __init__(self):
         """Constructs a new instance of Die with a value and points attribute.
 
         Args:
             self (Die): An instance of Die.
         """
+        self.value = 0
+        self.points = 0
 
 # 3) Create the roll(self) method. Use the following method comment.
+    def roll(self):
         """Generates a new random value and calculates the points.
         
         Args:
             self (Die): An instance of Die.
         """
+        self.value = random.randint(1, 6)
+        self.points = 50 if self.value == 5 else 100 if self.value == 1 else 0
